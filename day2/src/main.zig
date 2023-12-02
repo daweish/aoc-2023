620c426a4c2aaaa3dd0a8d3e994abb2686c24edc
Connor Walsh <connorw011@gmail.com>
2023-12-02 10:27:01 -0800

    day2 solution

--- day2/src/main.zig
+++ day2/src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
-const input = @embedFile("inputTest.txt");
+const input = @embedFile("input1.txt");
+//const input = @embedFile("inputTest.txt");
 const expect = std.testing.expect;
 
 const RGB = enum {
@@ -20,65 +21,58 @@ const RGB = enum {
     }
 };
 
-const GameResult = struct {
-    number: u32,
-    counts: [3]u32 = .{ 0, 0, 0 }, // red, green, blue
-};
-
 pub fn main() !void {
-
-    // Parse
-    // 1. Read in and split by line
-    // 2. Split each line into the Game# and Results
-    // 3. Split each reuslt into cube set
-    // 4.   for each cube set, total the number of r,g,g #might need these separate later
-    // 5. Record a game record
-
-    // 6. Check if the game record is possible with the given limits
-    // 7. Add to running sum
     const game_pre: []const u8 = "Game ";
+    const max_counts: [3]u32 = .{ 12, 13, 14 };
 
-    var line_iter = std.mem.tokenizeSequence(u8, input, "\n");
+    var valid_sum: u32 = 0;
+    var power_sum: u32 = 0;
+
+    var line_iter = std.mem.tokenizeSequence(u8, input, "\r\n"); // ugh windows
     while (line_iter.next()) |line| {
-
         // Split each line into Game and Results
         var line_split = std.mem.splitScalar(u8, line, ':');
         const game = line_split.next().?;
         try expect(std.mem.startsWith(u8, game, game_pre));
         const game_num = try std.fmt.parseInt(u32, game[game_pre.len..], 10); // skip "Game "
-
-        var game_result: GameResult = .{ .number = game_num };
-
         const results = line_split.next().?;
+
         var result_iter = std.mem.splitScalar(u8, results, ';');
+        var min_result = [_]u32{0} ** 3;
+        var possible = true;
+
         while (result_iter.next()) |result| {
+            var round_result = [_]u32{0} ** 3;
             var color_pairs = std.mem.splitScalar(u8, result, ',');
+
             while (color_pairs.next()) |pair| {
                 // there's an annoying space in front
                 var pair_iter = std.mem.splitScalar(u8, pair[1..], ' ');
 
-                const num = std.fmt.parseInt(u32, pair_iter.next().?, 10) catch {
-                    std.debug.print("Failed to parse int for pair: '{s}'\n", .{pair});
-                    return;
-                };
-
+                const num = try std.fmt.parseInt(u32, pair_iter.next().?, 10);
                 const color_str = pair_iter.next().?;
-                const color = RGB.fromString(color_str) orelse {
-                    std.debug.print("Failed to parse color:'{s}' with num {d} for line: '{s}'\n", .{ color_str, num, line });
-                    return;
-                };
-                game_result.counts[@intFromEnum(color)] += num;
+                const color = RGB.fromString(color_str) orelse return;
+                round_result[@intFromEnum(color)] += num;
+            }
+
+            for (max_counts, round_result, &min_result) |max, count, *min_count| {
+                min_count.* = @max(count, min_count.*);
+
+                if (max < count) {
+                    possible = false;
+                }
             }
         }
 
-        std.debug.print("Parsed game result: {any}\n", .{game_result});
+        // This game was valid
+        valid_sum += if (possible) game_num else 0;
+        power_sum += min_result[0] * min_result[1] * min_result[2];
     }
 
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
-    _ = stdout;
 
-    //try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    try stdout.print("valid game sum: {d}\ngame power sum: {d}\n", .{ valid_sum, power_sum });
     try bw.flush();
 }
